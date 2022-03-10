161760cc12e4892a2cb82b083be6141045148149
zougar12 <93253263+zougar12@users.noreply.github.com>
2022-03-10 14:54:05 +0100

    Update Programme.java

--- src/Programme.java
+++ src/Programme.java
@@ -12,9 +12,9 @@ public class Programme {
 	public static void main(String[] args) throws IOException {
 		
 		
-		String path = "C:\\Users\\louis\\Desktop\\web\\txtfiles\\";
-    	String chsavehtml = "C:\\Users\\louis\\Desktop\\web\\";
-        String chsavepasswd = "C:\\Users\\louis\\Desktop\\web\\";
+		String path = "https://github.com/zougar12/gosecuri/blob/main/mspr/cberthier.txt\\";
+    	String chsavehtml = "C:\\wamp\\www\\mspr\\";
+        String chsavepasswd = "C:\\wamp\\www\\mspr\\";
         
         
         Path path1;
