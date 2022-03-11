c81d347bd4b70480e1ea04bbdc826cd8f65bf27e
zougar12 <93253263+zougar12@users.noreply.github.com>
2022-03-11 10:31:26 +0100

    Update Programme.java

--- src/Programme.java
+++ src/Programme.java
@@ -12,9 +12,9 @@ public class Programme {
 	public static void main(String[] args) throws IOException {
 		
 		
-	String path = "..\\Nouveau dossier\\";
-    	String chsavehtml = "..\\Nouveau dossier\\";
-        String chsavepasswd = "..\\Nouveau dossier\\";     
+	String path = "C:\\Users\\zouga\\OneDrive\\Bureau\\Nouveau dossier\\";
+    	String chsavehtml = "C:\\wamp\\www\\mspr\\";
+        String chsavepasswd = "C:\\wamp\\www\\mspr\\";     
         
         
         Path path1;
